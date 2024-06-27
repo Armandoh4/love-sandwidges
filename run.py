b1310c5ba1fedf688d16ffe75fd1c2fc5f4a3c4e
Armando Halilaj <armandohalilaj4@hotmail.com>
2024-06-27 21:27:13 +0100

    Updates to link project to document

--- run.py
+++ run.py
@@ -1,22 +1,20 @@
-# import gspread
-# from google.oauth2.service_account import Credentials
+import gspread
+from google.oauth2.service_account import Credentials
 from pprint import pprint
-# SCOPE = [
-#     "https://www.googleapis.com/auth/spreadsheets",
-#     "https://www.googleapis.com/auth/drive.file",
-#     "https://www.googleapis.com/auth/drive"
-#     ]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-# CREDS = Credentials.from_service_account_file('creds.json')
-# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-# GSPRED_CLIENT = gspread.authorize(SCOPED_CREDS)
-# SHEET = GSPRED_CLIENT.open('love_sandwiches')
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPRED_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPRED_CLIENT.open('love_sandwiches')
 
-# sales = SHEET.worksheet('sales')
-
-# data = sales.get_all_values()
-
-# print(data)
+sales = SHEET.worksheet('sales')
+data = sales.get_all_values()
+print(data)
 
 def get_sales_data():
     """
@@ -28,7 +26,6 @@ def get_sales_data():
         print("example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-
         sales_data = data_str.split(",")
         
         if validate_data(sales_data):
@@ -51,7 +48,6 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-
     return True
 
 def update_sales_worksheet(data):
@@ -62,10 +58,15 @@ def update_sales_worksheet(data):
     print('Sales worksheet upsated successfully')
 
 def calculate_surplus_data(sales_row):
-    print('calculating surplus data\n')
+    print('Calculating surplus data\n')
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
-    # pprint(stock)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    print(surplus)
 
 def main():
     """
@@ -74,8 +75,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("welcome to love-sandwidges data automation")
 main()
-    
